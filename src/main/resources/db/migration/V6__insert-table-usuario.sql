dce50323a0e8ac191f571670d4b23f38acbf8758
Guilherme Henrique da Silva <ghs.guilherme.silva@gmail.com>
2023-09-13 14:46:08 -0300

    Added handling for main https. Username entered for login and insomnia file

---
+++ src/main/resources/db/migration/V6__insert-table-usuario.sql
@@ -0,0 +1,7 @@
+-- Login: admin
+-- Senha: 123456
+-- http://localhost:8080/login
+
+-- USUARIO
+INSERT INTO usuarios (login,senha) VALUES
+	 ('admin','$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.');
