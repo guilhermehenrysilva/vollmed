507006d0923c9b8506dac0e490e1d263757d10ff
Guilherme Henrique da Silva <ghs.guilherme.silva@gmail.com>
2023-11-27 17:14:50 -0300

    A new module was created to schedule and cancel appointments.

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,33 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultasService;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaController {
+
+    @Autowired
+    private AgendaDeConsultasService service;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        var dto = service.agendar(dados);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+        service.cancelar(dados);
+        return ResponseEntity.noContent().build();
+    }
+
+}
